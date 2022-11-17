305869ee7e12bbbaed8e67de050cf14bed795316
Max <maxageev5@gmail.com>
2022-11-17 12:25:44 +0300

    2

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class Generics {
 
     //TODO: Refactor Method-1
-    public <T>List boxingMethod(T name) {
+    public <T> List boxingMethod(T name) {
         List<T> firstList = new ArrayList<>();
         firstList.add(name);
-        List<List<T>> secondList = new ArrayList<>();
+        List<List<?>> secondList = new ArrayList<>();
         secondList.add(firstList);
         return secondList;
     }
@@ -17,11 +17,11 @@ public class Generics {
     //TODO: Refactor Method-2
     public <T> T genericMethod(T data) {
 
-        return (T) data;
+        return  data;
     }
 
     //TODO: Refactor Method-3
-    public <T,G>void cloneMethod(List<T> consumer, List<?extends T> producer) {
+    public <T>void cloneMethod(List<T> consumer, List<?extends T> producer) {
 
         consumer.addAll(producer);
     }
